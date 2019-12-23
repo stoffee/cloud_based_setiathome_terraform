37707de80fb4e9399be6a5d466a82ca196fdb661
stoffee <chris@roadkill.org>
2019-12-23 15:44:46 -0800

    errors in format

--- gcp/instance.tf
+++ gcp/instance.tf
@@ -28,7 +28,16 @@ resource "google_compute_instance" "default" {
     Mission = "Seti"
   }
 
-  metadata_startup_script = "echo hello_umm_hello > /test.txt"
+  #metadata_startup_script = "echo hello_umm_hello > /test.txt"
+  metadata_startup_script = <<SCRIPT
+    sudo apt update && sudo apt install -y unzip jq boinc-client dnsutils
+    sleep 12
+    systemctl restart boinc-client
+    sleep 12
+    sudo boinccmd --project_attach http://setiathome.berkeley.edu 51f200480b1473b1b972f89b051a31d3
+    sleep 12
+    systemctl restart boinc-client
+  SCRIPT
 
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
