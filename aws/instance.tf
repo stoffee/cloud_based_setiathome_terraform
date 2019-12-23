1bb9c30a8e18d6d9043db64d3cbdcd02446c7f03
stoffee <chris@roadkill.org>
2019-12-22 17:06:08 -0800

    changed vm size

--- aws/instance.tf
+++ aws/instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "demo" {
  ami = "ami-02c8040256f30fb45"
 
-   instance_type = "g4dn.xlarge"
+   instance_type = "t2.medium"
+   #instance_type = "g4dn.xlarge"
 
   key_name = "${aws_key_pair.env0-key-pair.key_name}"
 
