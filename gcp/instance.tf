2eb5738c9565c9f80446ee8308d01a2ac845c971
stoffee <chris@roadkill.org>
2021-04-01 12:39:24 -0700

    Update instance.tf

--- gcp/instance.tf
+++ gcp/instance.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "${random_pet.server.id}-wcg"
+  name         = "${random_pet.server.id}-proj"
   machine_type = var.instance_type
   zone         = var.gcp_zone
 
